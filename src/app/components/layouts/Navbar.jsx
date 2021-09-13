70b288bb3f1d5ef1bf6bf0894063b003578cd2aa
Rishabh Singh <ris8645208@gmail.com>
2021-09-13 17:59:42 +0530

    chore: add navbar components

--- src/app/components/layouts/Navbar.jsx
+++ src/app/components/layouts/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks'
 
 const Navbar = () => {
     return (
-        <nav className="nav wrapper grey darke-3">
+        <nav className="nav wrapper grey darken-3">
             <div className="container">
                 <Link to="/" className="brand-logo">Home</Link>
                 <SignedInLinks />
+                <SignedOutLinks />
             </div>
         </nav>
     )
