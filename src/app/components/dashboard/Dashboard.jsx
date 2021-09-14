f9348a1f4d9b8a05c7a01a8ef376af1eb122b25e
Rishabh Singh <ris8645208@gmail.com>
2021-09-14 21:45:05 +0530

    chore: add firestore to reducer

--- src/app/components/dashboard/Dashboard.jsx
+++ src/app/components/dashboard/Dashboard.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useContext } from 'react'
 import { ReactReduxContext } from 'react-redux'
 import { useSelector } from 'react-redux'
 import ProjectList from '../projects/ProjectList'
 import Notifactions from './Notifactions'
+import firebaseConfig from '../../config/firebase/firebaseConfig'
+import 'firebase/firestore';
 
 const Dashboard = () => {
     const { store } = useContext(ReactReduxContext);
+    let firestore = useSelector(state => { if (state) return state });
     const projects = useSelector(state => { if (state) return state.project });
-    console.log(store)
+    console.log(firestore)
+
+    const projectobj = firebaseConfig.firestore().collection('projects')
+    console.log(projectobj)
 
     return (
         <div className="dashboard container">
